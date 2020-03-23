20e66ef53458527c0f7e7d06349c54a093836fe7
Živilė Valiuškaitė <44140767+zivval2@users.noreply.github.com>
2020-03-23 22:11:59 +0200

    Working time registers FE

--- src/app/working-time-registers/shared/working-time-register.model.ts
+++ src/app/working-time-registers/shared/working-time-register.model.ts
@@ -1,2 +1,9 @@
 export class WorkingTimeRegister {
+    WorkingTimeRegisterId : number;
+    DateFrom : Date;
+    DateTo : Date;
+    Hours : number;
+    ProjectStageId : number;
+    EmployeeRoleId : number;
+    EmployeeId : number;
 }
--- src/app/working-time-registers/working-time-register-form/working-time-register-form.component.ts
+++ src/app/working-time-registers/working-time-register-form/working-time-register-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkingTimeRegisterService } from '../shared/working-time-register.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-working-time-register-form',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkingTimeRegisterFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: WorkingTimeRegisterService,
+    private toastr: ToastrService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.service.refreshProjectStageList();
+    this.service.refreshEmployeeRoleList();
+    this.service.refreshEmployeeList();
+    this.resetForm();
+    
+  }
+
+  resetForm(form?: NgForm)
+  {
+    if(form != null)
+      form.resetForm();
+    this.service.formData = {
+      WorkingTimeRegisterId : 0,
+      DateFrom : null,
+      DateTo : null,
+      Hours : 0,
+      ProjectStageId : 0,
+      EmployeeRoleId : 0,
+      EmployeeId : 0
+    }
+  }
+
+  onSubmit(form:NgForm){
+    if(this.service.formData.WorkingTimeRegisterId ==0)
+      this.insert(form);
+    else
+      this.update(form);
+  }
+
+  insert(form:NgForm)
+  {
+    this.service.postRecord().subscribe(
+      res => {
+        this.resetForm(form);
+        this.toastr.success('Įrašas sėkmingai pridėtas');
+        this.service.refreshList();
+      },
+      err => {
+        console.log(err)
+        this.toastr.error('Įvyko klaida');
+      }
+    )
+  }
+
+  update(form:NgForm)
+  {
+    this.service.putRecord().subscribe(
+      res => {
+        this.resetForm(form);
+        this.toastr.success('Įrašas sėkmingai atnaujintas');
+        this.service.refreshList();
+      },
+      err => {
+        console.log(err)
+        this.toastr.error('Įvyko klaida');
+      }
+    )
   }
 
 }
