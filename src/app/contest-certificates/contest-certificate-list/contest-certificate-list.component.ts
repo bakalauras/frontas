8cbd3e9abeec35c2f2f7b0c5403f043cc9b445f4
Skaiste Ramanauskaite <ramanauskaiteskaiste@gmail.com>
2020-04-20 15:02:18 +0300

    stageCompetency, contestCertificate, datos komponentas

--- src/app/contest-certificates/contest-certificate-list/contest-certificate-list.component.ts
+++ src/app/contest-certificates/contest-certificate-list/contest-certificate-list.component.ts
@@ -3,40 +3,97 @@ import { ContestCertificateService } from '../shared/contest-certificate.service
 import { ToastrService } from 'ngx-toastr';
 import { ContestCertificate } from '../shared/contest-certificate.model';
 import { Router, ActivatedRoute } from '@angular/router';
+import { KendoGridComponent } from 'src/app/kendo-grid/kendo-grid.component';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contest-certificate-list',
   templateUrl: './contest-certificate-list.component.html',
   styles: []
 })
-export class ContestCertificateListComponent implements OnInit {
+export class ContestCertificateListComponent extends KendoGridComponent implements OnInit {
 
   id = null;
   constructor(public service:ContestCertificateService, private toastr: ToastrService, private router: Router,
     public route: ActivatedRoute) { 
+      super();
       this.id = this.route.snapshot.paramMap.get('id'); //get id parameter
     }
 
   
   ngOnInit(){
-    this.service.refreshList(this.id);
+    this.service.refreshList(this.id, this.loadItems.bind(this));
+    this.service.refreshCertificateList();
+    this.resetForm();
   }
 
   populateForm(pd:ContestCertificate){
     this.service.formData = Object.assign({}, pd);
+    this.opened2 = true;
   }
 
-  onDelete(ContestCertificateId){
-    if(confirm('Ar tikrai norite ištrinti?')){
-      this.service.deleteContestCertificate(ContestCertificateId)
+  onDelete(){
+    this.opened = false;
+    if(this.idToDelete!=0){
+      this.service.deleteContestCertificate(this.idToDelete)
       .subscribe(res =>{
-        this.service.refreshList(this.id);
-        this.toastr.success('Ištrinta sėkmingai');
+        this.toastr.success('Įrašas sėkmingai ištrintas');
+        this.service.refreshList(this.id, this.loadItems.bind(this));
       },
         err => {
           console.log(err);
-          this.toastr.error('Įvyko klaida');
+          this.toastr.error(err.error);
         })
     }
+  }
+
+  resetForm(form?:NgForm) {
+    if(form!=null)
+      form.resetForm();
+    this.service.formData = {
+      ContestCertificateId: 0,
+      Amount: 0,
+      CertificateId: 0,
+      ContestId: this.id
+    }
+  }
+
+  onSubmit(form:NgForm)
+  {
+    if(this.service.formData.ContestCertificateId == 0)
+      this.insertRecord(form);
+    else
+      this.updateRecord(form);
+    this.close();
+  }
+
+  insertRecord(form:NgForm)
+  {
+    this.service.postContestCertificate().subscribe(
+      res => {
+        this.resetForm(form),
+        this.toastr.success('Įrašas sėkmingai pridėtas');
+        this.service.refreshList(this.id, this.loadItems.bind(this));
+      },
+      err => {
+        console.log(err);
+        this.toastr.error(err.error);
+      }
+    )
+  }
+
+  updateRecord(form:NgForm)
+  {
+    this.service.putContestCertificate().subscribe(
+      res => {
+        this.resetForm(form),
+        this.toastr.success('Įrašas sėkmingai atnaujintas');
+        this.service.refreshList(this.id, this.loadItems.bind(this));
+      },
+      err => {
+        console.log(err);
+        this.toastr.error(err.error);
+      }
+    )
   }
 }
--- src/app/stage-competencies/shared/stage-competency.service.ts
+++ src/app/stage-competencies/shared/stage-competency.service.ts
@@ -20,12 +20,11 @@ export class StageCompetencyService {
 
   constructor(private http:HttpClient) { }
 
-  postStageCompetency(formData:StageCompetency){
+  postStageCompetency(){
     return this.http.post(this.rootURL + '/StageCompetencies', this.formData)
   }
 
-  putStageCompetency(formData:StageCompetency){
-    console.log(formData);
+  putStageCompetency(){
     return this.http.put(this.rootURL + '/StageCompetencies/'+ this.formData.StageCompetencyId, this.formData)
   }
 
@@ -33,19 +32,13 @@ export class StageCompetencyService {
     return this.http.delete(this.rootURL + '/StageCompetencies/'+ id)
   }
 
-  //refreshList(){
-  //  this.http.get(this.rootURL + '/StageCompetencies')
- //   .toPromise()
-  //  .then(res =>this.list=res as StageCompetency[])
- // }
-
-  refreshList(id)
+  refreshList(id, callBack)
   {
     if(id!=0)
     {
       this.http.get(this.rootURL+'/ProjectStages/'+id+'/competencies').
     toPromise()
-    .then(res => this.list = res as StageCompetency[]);
+    .then(res => {this.list=res as StageCompetency[], callBack(this)});
     }
   }
 
@@ -55,26 +48,6 @@ export class StageCompetencyService {
     toPromise()
     .then(res => this.competencyList = res as Competency[]);
   }
-
-  //refreshStageList()
- // {
- //   this.http.get(this.rootURL+'/ProjectStages').
- //   toPromise()
- //   .then(res => this.stageList = res as ProjectStage[]);
- // }
-
- // refreshStageNameList()
- // {
- //   this.http.get(this.rootURL+'/ProjectStageNames').
- //   toPromise()
-//    .then(res => this.stageNameList = res as ProjectStageName[]);
-//  }
-
- // getProjectStageName(id)
- // {
- //     var nr = this.stageList.find(x => x.ProjectStageId == id).ProjectStageNameId;
- //      return this.stageNameList.find(x => x.ProjctStageNameId == nr).StageName;
-//  }
   
   getCompetencyTitle(id)
   {
